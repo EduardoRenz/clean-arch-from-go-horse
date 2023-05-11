5ea3ec4c23158e8041ceaf3f484648aff4c52ce1
Eduardo Renz <dudarenz@gmail.com>
2023-05-11 19:54:36 -0300

    added express to application

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello world!")
+import express from 'express'
+const app = express()
+const port = 5000
+app.get('/', (_, res) => {
+  res.status(200).json({ Hello: 'World' }).send()
+})
+app.listen(port, () => console.log(`Running on port ${port}`))
