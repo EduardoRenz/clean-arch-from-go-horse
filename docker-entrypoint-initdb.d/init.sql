3b63eda3a54e43110539d29356fc41d6ddbfc537
Eduardo Renz <dudarenz@gmail.com>
2023-05-11 21:19:25 -0300

    configured docker to create a pg databaase

---
+++ docker-entrypoint-initdb.d/init.sql
@@ -0,0 +1,31 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
+);
+
+
+/** 
+Creates a table representing a users wallet, that contains the currency type and the amount in his currency
+the currency is a column of type ENUM with BRL and BTC
+**/
+CREATE TYPE wallet_currency_type AS ENUM ('BRL', 'BTC');
+CREATE TABLE wallet (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  currency wallet_currency_type NOT NULL,
+  amount NUMERIC(10, 8) NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+/**
+Create a table transaction with timestamps and the amount of the transaction, and the currency of the transaction
+**/
+CREATE TABLE transaction (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  currency  wallet_currency_type NOT NULL,
+  amount NUMERIC(10, 8) NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
