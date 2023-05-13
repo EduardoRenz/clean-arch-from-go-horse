a25eb6a9302f6179f3cfba85dd138557eae6c75f
Eduardo Renz <dudarenz@gmail.com>
2023-05-13 14:15:10 -0300

    starting gohorse

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
 import express from 'express'
 const app = express()
 const port = 5000
+
+import { Connection } from 'postgresql-client'
+
+const user = 'user'
+const password = 'user'
+const dbName = 'postgres'
+
+const connection = new Connection(`postgres://${user}:${password}@localhost/${dbName}`)
+
 app.get('/', (_, res) => {
   res.status(200).json({ Hello: 'World' }).send()
 })
+
+app.get('/wallet', async (_, res) => {
+  // suposes to get a user id from a token
+  const userId = 1
+
+  await connection.connect()
+  const result = await connection.query('SELECT * FROM wallet where user_id = $1', { params: [userId] })
+  await connection.close()
+  res.status(200).json(result.rows).send()
+})
+
 app.listen(port, () => console.log(`Running on port ${port}`))
