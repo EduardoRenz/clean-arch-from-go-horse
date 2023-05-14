1b05d70589b8e53cd2e9002aca266fc875c2a921
Eduardo Renz <dudarenz@gmail.com>
2023-05-14 19:46:21 -0300

    optional preferred currency

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -3,11 +3,11 @@ import { Currencies } from './common'
 export default class User {
   id: number
   name: String
-  preffered_currency: Currencies
+  preffered_currency: Currencies = Currencies.BRL
 
-  constructor(id: number, name: String, preffered_currency: Currencies) {
+  constructor(id: number, name: String, preffered_currency?: Currencies) {
     this.id = id
     this.name = name
-    this.preffered_currency = preffered_currency
+    if (preffered_currency != null) this.preffered_currency = preffered_currency
   }
 }
