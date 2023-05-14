80656fcc6a6d67199d50eed225165d305fbcf5a0
Eduardo Renz <dudarenz@gmail.com>
2023-05-13 21:52:34 -0300

    created wallet

---
+++ src/entities/Wallet.ts
@@ -0,0 +1,35 @@
+import { Currencies } from './common'
+
+export class WalletCurrency {
+  currency: Currencies
+  amount: number
+
+  constructor(currency: Currencies, amount: number) {
+    this.currency = currency
+    this.amount = amount
+  }
+
+  public toObject() {
+    return {
+      currency: this.currency,
+      amount: this.amount
+    }
+  }
+}
+
+export default class {
+  ownerId: number
+  currencies: WalletCurrency[]
+
+  constructor(ownerId: number, currencies: WalletCurrency[]) {
+    this.ownerId = ownerId
+    this.currencies = currencies
+  }
+
+  public toObject() {
+    return {
+      ownerId: this.ownerId,
+      currencies: this.currencies.map((currency) => currency.toObject())
+    }
+  }
+}
