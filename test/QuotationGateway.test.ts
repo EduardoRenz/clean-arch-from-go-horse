1376e293972b0ab02c35252d44272051b79ead3c
Eduardo Renz <dudarenz@gmail.com>
2023-05-14 22:47:48 -0300

    compare two cripto endpoints

--- test/QuotationGateway.test.ts
+++ test/QuotationGateway.test.ts
@@ -5,14 +5,25 @@ import QuotationBinanceGateway from '../src/gateways/QuotationBinanceGateway'
 
 describe('Quotation gateways tests', () => {
   it('Should get a user preferred currency and be BTC', async () => {
-    //const cryptocompareGateway = new QuotationCryptoCompareGateway()
-    const cryptocompareGateway = new QuotationBinanceGateway()
-    const btcBrl = await cryptocompareGateway.get(Currency.BTC, Currency.BRL)
-    expect(btcBrl).not.toBeNaN()
-    expect(btcBrl).toBeGreaterThan(0)
+    const cryptocompareGateway = new QuotationCryptoCompareGateway()
+    const binanceGateway = new QuotationBinanceGateway()
+    const compareBTCBRL = await cryptocompareGateway.get(Currency.BTC, Currency.BRL)
+    expect(compareBTCBRL).not.toBeNaN()
+    expect(compareBTCBRL).toBeGreaterThan(0)
 
-    const brlBtc = await cryptocompareGateway.get(Currency.BRL, Currency.BTC)
-    expect(brlBtc).not.toBeNaN()
-    expect(brlBtc).toBeGreaterThan(0)
+    const compareBRLBTC = await cryptocompareGateway.get(Currency.BRL, Currency.BTC)
+    expect(compareBRLBTC).not.toBeNaN()
+    expect(compareBRLBTC).toBeGreaterThan(0)
+
+    const binanceBTCBRL = await binanceGateway.get(Currency.BTC, Currency.BRL)
+    expect(binanceBTCBRL).not.toBeNaN()
+    expect(binanceBTCBRL).toBeGreaterThan(0)
+
+    const binanceBRLBTC = await binanceGateway.get(Currency.BRL, Currency.BTC)
+    expect(binanceBRLBTC).not.toBeNaN()
+    expect(binanceBRLBTC).toBeGreaterThan(0)
+
+    expect(compareBRLBTC).toBeCloseTo(binanceBRLBTC)
+    expect(Math.abs(compareBTCBRL - binanceBTCBRL)).toBeLessThan(100)
   })
 })
