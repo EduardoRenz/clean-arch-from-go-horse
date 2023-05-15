8aec52400c3ec6eac2b93a1b2d9d6802b4c4b865
Eduardo Renz <dudarenz@gmail.com>
2023-05-15 00:19:07 -0300

    removed whit spaces

--- src/usecases/PurchaseController.ts
+++ src/usecases/PurchaseController.ts
@@ -31,22 +31,15 @@ export default class PurchaseController {
   }
 
   async purchase(userId: number, purchaseData: PurchaseParams): Promise<number> {
-    // Get user prefered currency
     const prefered_currency = await this.userRepository.getPreferredCurrency(userId)
-    // Get the data from the request
-
     const current_quotation = await this.quotationGateway.get(purchaseData.currency, prefered_currency)
-
     const amount = purchaseData.amount * current_quotation
     const currencyBalance = await this.walletRepository.getCurrencyBalance(userId, prefered_currency)
     if (currencyBalance.amount < amount) throw new Error('insufficient balance')
-
     const new_transaction = await this.transactionRepository.createTransaction(
       new Transaction(userId, purchaseData.currency, purchaseData.amount, prefered_currency, amount, 'DEBIT')
     )
-
     await this.walletRepository.updateCurrencyBalance(userId, prefered_currency, currencyBalance.amount - amount)
-
     return new_transaction
   }
 }
